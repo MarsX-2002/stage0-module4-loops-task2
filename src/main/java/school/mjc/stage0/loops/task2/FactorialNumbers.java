99cf85bd1958a0035c5b319262c1746badf388f2
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-30 15:46:50 +0500

    while loop

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,17 +1,23 @@
 package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
+
+
     public void printFactorialRow(int printToInclusive) {
         int factorial = 1;
-        int count = 1;
-        if (printToInclusive == 0) {
-            System.out.println(factorial);
-        } else {
-            while (count <= printToInclusive) {
+        int count = 0;
+
+        while (count <= printToInclusive) {
+            if (count == 0) {
+                System.out.println(1);
+            } else {
+
                 factorial *= count;
                 System.out.println(factorial);
-                count++;
+
             }
+            count++;
+
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,9 +3,12 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int counter = 0;
-        while (counter < Math.abs(multiplyByAndToInclusive)){
-            System.out.println(counter * multiplyByAndToInclusive);
-            counter++;
+        if (multiplyByAndToInclusive != 0) {
+
+            while (counter <= Math.abs(multiplyByAndToInclusive)) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
         }
     }
 }
