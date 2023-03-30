56a7963bb8eb31031b9403dca3e63172181fed1d
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-30 11:39:49 +0500

    while loop

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        boolean isPrime = true;
+        if (printToInclusive >= 2) {
+            while (counter <= printToInclusive) {
+//                checking if it is prime
+//                e.i 2 * 3 = 6
+//                2 <= sqrt(6)
+                for (int i = 2; i <= Math.sqrt(counter); i++) {
+                    if (counter % i == 0) {
+                        isPrime = false;
+                    }
+                }
+
+                if (isPrime) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        }
     }
 }
